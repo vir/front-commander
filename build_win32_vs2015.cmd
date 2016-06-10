0cb45bec0f98fd59e7ed0b3dc922bb65de55c79e
Vasily i. Redkin <vir@ctm.ru>
2016-06-10 12:13:45 +0300

    Added windows build script

---
+++ build_win32_vs2015.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+
+set VSVER=14.0
+
+
+echo.
+echo Searching for VisualStudio setup folders
+reg.exe query HKLM\Software\Wow6432Node > nul 2>&1
+if errorlevel 1 (
+	echo "We are at 32bit system"
+	set wow=
+) else (
+	echo "We are at 64bit system, good :-)"
+	set wow=\Wow6432Node
+)
+
+for /f "tokens=2*" %%a in ('reg.exe query HKLM\Software%wow%\Microsoft\VisualStudio\%VSVER%\Setup\VS /v EnvironmentPath') do set DEVENV=%%b
+for /f "tokens=2*" %%a in ('reg.exe query HKLM\Software%wow%\Microsoft\VisualStudio\%VSVER%\Setup\VS /v ProductDir') do set PRODDIR=%%b
+echo PRODDIR: %PRODDIR%
+echo DEVENV: %DEVENV%
+
+set THISDIR=%~dp0
+set VSDIR="%PRODDIR%\VC"
+
+call %VSDIR%\vcvarsall.bat x86
+
+cl /EHsc game.cxx
+
